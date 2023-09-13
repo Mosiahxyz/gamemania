23f8ba3a85ab06fef7afe17d883fbf6516bdfb2c
Mosiah <127303388+Mosiahxyz@users.noreply.github.com>
2023-09-13 11:38:16 -0300

    Add files via upload

---
+++ src/script.ts
@@ -0,0 +1,35 @@
+class Menu extends HTMLElement {
+
+    constructor() {
+        
+        super();
+        this.innerHTML = `
+        
+        <header>
+        
+        <nav>
+  
+  
+              <img src="./img/logo.png"  id="logo" >
+              
+  
+           <ul class="menu">
+  
+               <li><a href="#">HOME</a></li>
+               <li><a href="./pages/configuracoes.html">REQUISITOS</a></li>
+              <li><a href="./pages/faq.html">FAQ</a>
+              <li><a href="./pages/sobre.html">SOBRE</a></li>
+  
+           </ul>
+         </nav>
+  
+      </header>
+
+
+    `
+    
+
+    }
+}
+
+    customElements.define(`meu-menu`, Menu);
